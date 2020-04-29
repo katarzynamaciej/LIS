ec4caea32f445eb981e112a6f0d186927053383f
maciejk <maciejk@ryanair.com>
2020-04-29 16:57:14 +0200

    fix formatting

--- LIS.go
+++ LIS.go
@@ -1,45 +1,44 @@
 package main
 
-
-func FindLIS(input []int) []int{
-	size:=len(input)
-	if size ==0 {
+func FindLIS(input []int) []int {
+	size := len(input)
+	if size == 0 {
 		return input
 	}
 	maxLength := 1
-	P := make([]int, size)
-	M := make([]int, size + 1)
+	p := make([]int, size)
+	m := make([]int, size+1)
 
 	for i := 1; i < size; i++ {
 		lo := 1
 		hi := maxLength
 		for lo <= hi {
-			mid := ceil(float64((lo+hi)/2))
-			if input[M[mid]] < input[i] {
+			mid := ceil(float64((lo + hi) / 2))
+			if input[m[mid]] < input[i] {
 				lo = mid + 1
 			} else {
-				hi = mid-1
+				hi = mid - 1
 			}
 		}
 
-		P[i] = M[lo-1]
-		M[lo] = i
+		p[i] = m[lo-1]
+		m[lo] = i
 
-		if lo> maxLength {
+		if lo > maxLength {
 			maxLength = lo
 		}
 
 	}
 	lis := make([]int, maxLength)
-	indexOfElementOfLIS := M[maxLength]
-	for i := maxLength-1; i >= 0; i--{
+	indexOfElementOfLIS := m[maxLength]
+	for i := maxLength - 1; i >= 0; i-- {
 		lis[i] = input[indexOfElementOfLIS]
-		indexOfElementOfLIS = P[indexOfElementOfLIS]
+		indexOfElementOfLIS = p[indexOfElementOfLIS]
 	}
 	return lis
 }
 
-func ceil(num float64) int{
+func ceil(num float64) int {
 	iNum := int(num)
 	if num == float64(iNum) {
 		return iNum
